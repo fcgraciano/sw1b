1591ee9f128466f22df6a2ac621182451bd83d83
LuisFilipeTinos <lftinos@gmail.com>
2025-10-21 16:31:07 -0300

    Instalação do menu

--- index.php
+++ index.php
@@ -1,97 +1,27 @@
 <!DOCTYPE html>
-<html lang="pt-BR">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Página Inicial</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/sidebar/sidebars.css" rel="stylesheet" />
 </head>
 <body>
-
-<h1>Echo</h1>
-<?php echo "Escrevendo na tela"; ?>
-
-<?php $nome = "Prof Fernando"; ?>
-
-<h2>If</h2>
-<?php 
-    if($nome == "Prof Fernando")
-    {
-        echo "É sim o prof f***";
-    }else{
-        echo "Não é o prof certo";
-    }
-?>
-
-<h2>Listas, Array chave Valor</h2>
-<?php 
-
-    $minhaLista = [];
-    $minhaLista[] = "Item 1";
-    $minhaLista[] = "Laranja";
-    $minhaLista[] = "Gol 1.6";
-    $minhaLista[] = "Casa Container";
-
-    echo "<pre>";
-    print_r($minhaLista);
-    echo "</pre>";
-
-    echo "<ol>";
-    for($i = 0; $i < count($minhaLista); $i++)
-    {
-        echo "<li> $minhaLista[$i] </li>";
-    }
-    echo "</ol>";
-
-    $i = 0;
-    while($i < count($minhaLista))
-    {
-        echo "<h3> $minhaLista[$i] </h3>";
-        $i++;
-    }
-
-    echo "<table border='2'>";
-    foreach($minhaLista as $item)
-    {
-        echo "<tr><td>$item</td></tr>";
-    }
-    echo "</table>";
-
-    $professores = [];
-    $professores[] = [
-                        "Id"=>"1",
-                        "Nome"=>"Fernando",
-                        "Ativo"=>"1" 
-                    ];
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        Listagem
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 
 
-    $professores[] =  [
-                        "Id"=>"2",
-                        "Nome"=>"Ana Paula",
-                        "Ativo"=>"1" 
-                      ];
-
-    $professores[] =  [
-                        "Id"=>"3",
-                        "Nome"=>"Galvani",
-                        "Ativo"=>"0" 
-                      ];
-   echo "<table border='2'>";                
-   foreach($professores as $professor)
-   {
-        echo "<tr>
-                <td>$professor[Id]</td>
-                <td>$professor[Nome]</td>
-                <td>$professor[Ativo]</td>
-            </tr>";
-   }               
-   echo "</table>";
-
-
-
-
-
-
-?>
-
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="sidebars.js" class="astro-vvvwv3sm"></script>
 </body>
 </html>
